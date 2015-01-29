15209c0aa89497aa63a5381dd9b04f7495d79182
blaab2 <daniel@ossig-mail.de>
2015-01-29 11:20:52 +0100

    normverbindungen 2

--- dossig_normverbindungen/javaRules/expandThreadTrack.java
+++ dossig_normverbindungen/javaRules/expandThreadTrack.java
@@ -1,5 +1,7 @@
 import java.util.Collection;
 
+import org.eclipse.uml2.uml.Stereotype;
+
 import de.iils.dc43.scriptrule.InstanceWrapperExtensions;
 import de.iils.dc43.transformationengine.javarule.JavaRule;
 import de.iils.dc43.transformationengine.popup.actions.TransformationRunner;
@@ -11,18 +13,21 @@ public class expandThreadTrack extends JavaRule {
 	@Override
 	public void execute(TransformationRunner trafoRunner) {
 		Collection<ThreadTrack> allThreadTracks = InstanceWrapperExtensions.allInstances(ThreadTrack.class);
+		Stereotype spgconstStereo = getProfile(get43Profiles().getSpg()).getOwnedStereotype("spgconst");
+
 		for (ThreadTrack threadTrack : allThreadTracks) {
 			if (threadTrack.getExpanded().doubleValue() == 0) {
 				ThreadTrackPoint prevpoint = null;
 				for (int i = 0; i < threadTrack.getPoints(); i++) {
-					ThreadTrackPoint point = InstanceWrapperExtensions.createInstance(ThreadTrackPoint.class, threadTrack.umlInstance().getName() + "_Toint"
-							+ i);
+					ThreadTrackPoint point = InstanceWrapperExtensions
+							.createInstance(ThreadTrackPoint.class, threadTrack.umlInstance().getName() + "Point" + i);
 					threadTrack.element_add_(point);
 					threadTrack.threadTrackPoint_add_(point);
 					point.setThreadtrack(threadTrack);
 					if (i == 0) {
 						threadTrack.setStart(point);
-						point.setPoint(1.);
+						point.setPointnumber(1.0);
+						point.umlSlotPointnumber().applyStereotype(spgconstStereo);
 					}
 					if (i != 0) {
 						prevpoint.nextThreadTrackPoint_(point);
